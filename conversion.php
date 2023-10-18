195cd8cd0941cf80d1703a8c2cba9db60fbff2b5
unknown <kafouigisele@gmail.com>
2023-10-18 03:57:08 +0000

    rangement de l'history par date

--- conversion.php
+++ conversion.php
@@ -7,7 +7,7 @@ function convertirFCFAenEuro($montantFCFA) {
 
 echo ' <form action="" method="post">
 <input type="nomber" placeholder="Entrer le montant a convertir" id="nombre1" name="nombre1" step="1" required    style="color: blue; font-size: 16px;height: 50px;wight: 5px; text-align: center;"><br><br><br> 
-<input type="submit" value="CONVERSION" style="background-color: green; font-size: 16px;height: 50px;wight: 5px;"> <br><br><br><br>';
+<input type="submit" value="CONVERSION" style="background-color: green; color: white ; font-size: 16px;height: 50px;wight: 5px;"> <br><br><br><br>';
 
 
 
@@ -27,38 +27,51 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         }
 
 } 
-
-
-
-session_start(); // Démarre la session (si ce n'est pas déjà fait)
+session_start();
 
 // Obtenez la date actuelle (heure, jour et mois)
-$dateActuelle = date('H:i d M');
-$montantEnEuro = convertirFCFAenEuro($nombre);
+$dateActuelle = date('d M');
 
 // Ajoutez la conversion avec la date à l'historique
-$_SESSION['historique'][] = array(
+$_SESSION['historique'][] = array('Date' => $dateActuelle, 'Montant en FCFA à Convertir' => $nombre, 'Montant de Conversion' => $montantEnEuro);
 
-    'Date' => $dateActuelle,
-    'Montant en FCFA à Convertir' => $nombre,
-    'Montant de Conversion' => $montantEnEuro
-);
-
-if (!empty($_SESSION['historique'])) {
+if (!empty($_SESSION['historique']))
+ {
     echo '<H2>Historique des Conversions</H2>';
-    echo '<ul>';
-    foreach ($_SESSION['historique'] as $conversion) {
-        echo '<li>';
-        echo 'Date : ' . $conversion['Date'] . ', ';
-        echo 'Montant en FCFA à Convertir = ' . $conversion['Montant en FCFA à Convertir'] . ', ';
-        echo 'Montant de Conversion = ' . $conversion['Montant de Conversion'];
-        echo '</li>';
+    
+    // Créez un tableau pour stocker les conversions par date
+    
+    $conversionsParDate = array();
+
+    foreach ($_SESSION['historique'] as $conversion)
+     {
+        // Obtenez la date de la conversion
+        $dateConversion = $conversion['Date'];
+        // $dateConversion = substr($conversion['Date'],7,3); // Extrait "d M" (jour et mois)
+        
+        // Ajoutez la conversion au tableau correspondant à la date
+        $conversionsParDate[$dateConversion][] = $conversion;
     }
-    echo '</ul>';
-} else {
-    echo 'L\'historique est vide.';
+
+    
 }
 
-
+   
+// Parcourez les conversions par date et affichez-les
+    foreach ($conversionsParDate as $date => $conversions) 
+{
+        echo "<h3>Conversions du $date :</h3>";
+        
+       
+        echo '<ul>';
+        foreach ($conversions as $conversion) 
+        {
+            echo '<li>';
+            echo 'Le Montant en FCFA à Convertir = ' . $conversion['Montant en FCFA à Convertir'] . ', ';
+            echo 'Montant de Conversion = ' . $conversion['Montant de Conversion'];
+            echo '</li>';
+        }
+        echo '</ul>';     
+}
 
 ?>
